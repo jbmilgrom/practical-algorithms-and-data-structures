ce5ec94c3d2ec9362b4d362f629690a85c6ff38c
Jonathan Milgrom <jbmilgrom@gmail.com>
2018-08-04 08:38:00 -0700

    up and runnig in python

---
+++ hello.py
@@ -0,0 +1,4 @@
+arr = [1, 2, 3]
+
+for i, num in enumerate(arr):
+  print arr[i]
---
+++ sum_n.py
@@ -0,0 +1,18 @@
+def sumIterativeLoop(n):
+  sum = 0
+  for i in range(n + 1):
+    sum = sum + i
+  return sum
+
+print "sumIterativeLoop(3)"
+print sumIterativeLoop(3)
+
+def sumIterativeRecursive(n):
+  def iterate(n, sum):
+    if(n == 0):
+      return sum
+    return iterate(n - 1, sum + n)
+  return iterate(n, 0)
+
+print "sumIterativeRecursive(3)"
+print sumIterativeRecursive(3)
