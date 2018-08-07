a8a9f1d408fea392a71ce7b877df10978768ca7f
Jonathan Milgrom <jbmilgrom@gmail.com>
2018-08-07 16:34:21 -0700

    better naming

--- analysis/spiral.js
+++ analysis/spiral.js
@@ -8,18 +8,18 @@ const DIRECTIONS = [[0, 1] /* E */, [1, 0] /* N */, [0, -1] /* W */, [-1, 0] /* 
 const spiral = size => {
   let coord = [0, 0];
   const matrix = generateMatrixOfSize(size);
-  const getNextCoordinates = makeGetNextCoordintes(size, DIRECTIONS);
+  const compass = makeCompass(size, DIRECTIONS);
   const entries = size * size;
   for (let entry = 1; entry <= entries; entry++) {
     matrix[coord[0]][coord[1]] = entry;
-    coord = getNextCoordinates(matrix, coord);
+    coord = compass(matrix, coord);
   }
   return matrix;
 };
 
-const makeGetNextCoordintes = (size, directions) => {
+const makeCompass = (boundary, directions) => {
   let dir = 0;
-  const isOutOfBounds = m => m < 0 || m === size;
+  const isOutOfBounds = m => m < 0 || m === boundary;
   return (matrix, coord) => {
     let next = addTuples(coord, directions[dir]);
     if (isOutOfBounds(next[0]) || isOutOfBounds(next[1]) || matrix[next[0]][next[1]] !== undefined) {
