f63e2d07d564306f92d9a4f868bd319cc72bfdc7
Jonathan Milgrom <jbmilgrom@gmail.com>
2018-08-13 16:43:05 -0700

    clean up

--- search/binary_search.py
+++ search/binary_search.py
@@ -9,25 +9,27 @@ def binary_search(sortedList, item):
   length = len(sortedList)
   if length == 0:
     return False
-  def iterate(sortedList, item, start, finish):
-    if start == finish:
-      return item == sortedList[start]
-    # this rounds up
-    #   5 items will result index 5/2 => the 3rd item (midpoint)
-    #   4 items, will result in index 4/2 => the 3rd item (round up)
-    mid = start + (1 + finish - start) // 2
-    marker = sortedList[mid]
-    if item == marker:
-      return True
-    if item < marker:
-      # since we round up above we need to subtract 1 from finish here (as opposed to adding 1 to finish below)
-      # that is because of the case where n == 2
-      #   if item is less than the mid (which is 2 bc we are rounding up) we want to make sure to exclude 2
-      #   from the next slice
-      return iterate(sortedList, item, start, mid - 1)
-    else:
-      return iterate(sortedList, item, mid, finish)
-  return iterate(sortedList, item, 0, length - 1)
+  return iterate_binary_search(sortedList, item, 0, length - 1)
+
+# @private
+def iterate_binary_search(sortedList, item, start, finish):
+  if start == finish:
+    return item == sortedList[start]
+  # this rounds up
+  #   5 items will result index 5/2 => the 3rd item (midpoint)
+  #   4 items, will result in index 4/2 => the 3rd item (round up)
+  mid = start + (1 + finish - start) // 2
+  marker = sortedList[mid]
+  if item == marker:
+    return True
+  if item < marker:
+    # since we round up above we need to subtract 1 from finish here (as opposed to adding 1 to finish below)
+    # that is because of the case where n == 2
+    #   if item is less than the mid (which is 2 bc we are rounding up) we want to make sure to exclude 2
+    #   from the next slice
+    return iterate_binary_search(sortedList, item, start, mid - 1)
+  else:
+    return iterate_binary_search(sortedList, item, mid, finish)
 
 # TEST
 
