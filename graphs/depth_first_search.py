502c304df665f945f19d69bab9bbd270bf5cfa2f
Jonathan Milgrom <jbmilgrom@gmail.com>
2018-08-19 04:47:25 -0700

    comment up recursive DFS

--- graphs/depth_first_search.py
+++ graphs/depth_first_search.py
@@ -14,6 +14,27 @@ def depth_first_search(root, isItem):
       continue
   return False
 
+'''
+Steps:
+  Root is checked for item
+  The left-most child is checked for item
+  The left-most child's left-most child is checked for item
+  ...
+  If item is not found in the left-most branch's leaf-node
+  The leftest leaf-node's parent's next leftest child is checked
+  ...
+  If item is not found in the leftest leaf-node's parent's next leftest branch's leaf-node
+  And the leftest leaf-node's parent has no more children
+  the leftest leaf-node's grandparent's next leftest child is checked
+  ...
+
+The Call Stack Stores "Next Node" Instructions:
+When a branch is exhausted, the function call to the leaf node returns and is
+popped from the call stack, and the remainder of the call stack is summoned for further
+instruction. If the recursive process has been correctly designed, requests to children
+of increasingly senior nodes lay in wait as was all as increasingly junior nodes
+with respect thereto.
+'''
 def depth_first_search_recursive(root, isItem):
   if isItem(root['val']):
     return True
