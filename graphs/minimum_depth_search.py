d143516ce4673d906a72cfda2850f97ddf6e8b49
Jonathan Milgrom <jbmilgrom@gmail.com>
2018-08-19 22:36:08 -0700

    assign level data

--- graphs/minimum_depth_search.py
+++ graphs/minimum_depth_search.py
@@ -1,11 +1,43 @@
 from stacks.queue import Queue
+from pprint import pprint
 
 '''
 Ideas:
   Execute breadth first search
-  When an end is found, return depth
+    But how to the level children are associated with?
+      assign levels to nodes
+        root node is level 1
+        children are parent + 1
+      first node without children, level
+Queue analysis:
+  e.g
+        3
+       / \
+      9  20
+     / \
+    15  7
+
+  while queue [Node3]
+    queue is []
+    process(Node3)
+    enqueue Node9, Node20
+  while queue [Node9, Node20]
+    queue is [Node20]
+    process(Node9)
+    enqueue Node15, Node7
+  while queue [Node20, Node15, Node7]
+    queue is []
+    process(Node20)
+    enqueue nothing
+  while queue [Node15, Node7]
+    queue is [Node7]
+    process(Node15)
+    enqueue nothing
+  while queue [Node7]
+    queue is []
+    process(Node7)
+    enqueue nothing
 Steps:
-  Store number of levels traversed
   BFS:
     While queue has items
       dequeue item and check for children
@@ -13,16 +45,19 @@ Steps:
       if doesn't have children, return level
 '''
 def minimum_depth(root):
+  if not root:
+    return 0
   q = Queue()
-  q.enqueue(root)
-  level = 0
+  q.enqueue(node_level_pair(root, 1))
   while not q.is_empty():
-    level += 1
     node = q.dequeue()
-    left, right = node['left'], node['right']
-    if left is None and right is None:
+    left, right, level = node['node']['left'], node['node']['right'], node['level']
+    if not left and not right:
       return level
-    if left is not None:
-      q.enqueue(left)
-    else:
-      q.enqueue(right)
+    if left:
+      q.enqueue(node_level_pair(left, level + 1))
+    if right:
+      q.enqueue(node_level_pair(right, level + 1))
+
+def node_level_pair(node, level):
+  return {'node': node, 'level': level}
--- graphs/minimum_depth_search_test.py
+++ graphs/minimum_depth_search_test.py
@@ -7,7 +7,7 @@ from graphs.minimum_depth_search import minimum_depth
     /  \
    15   7
 '''
-BINARY_TREE = {
+BINARY_TREE_1 = {
   'val': 3,
   'left': {
     'val': 9,
@@ -29,9 +29,51 @@ BINARY_TREE = {
   }
 }
 
+BINARY_TREE_2 = {
+  'left': {
+    'left': {
+      'left': {
+        'left': None,
+        'right': {
+          'left': None,
+          'right': None,
+          'val': 2
+        },
+        'val': 2
+      },
+      'right': {
+        'left': None,
+        'right': {
+          'left': None,
+          'right': None,
+          'val': 4
+        },
+        'val': 3
+      },
+      'val': 3
+    },
+    'right': None,
+    'val': 6
+  },
+  'right': {
+    'left': None,
+    'right': {
+      'left': None,
+      'right': None,
+      'val': 67
+    },
+    'val': 9
+  },
+  'val': 7
+}
+
+
 print('############################')
 print('Testing minimum_depth_search')
 print('############################')
 
-min = minimum_depth(BINARY_TREE)
+min = minimum_depth(BINARY_TREE_1)
 assert min == 2, "Tree should have minimum depth of 2 and instead got %r" % min
+
+min = minimum_depth(BINARY_TREE_2)
+assert min == 3, "Tree should have minimum depth of 3 and instead got %r" % min
