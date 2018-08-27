f162abded1ccf3ce32e95cada725261e32f712c9
Jonathan Milgrom <jbmilgrom@gmail.com>
2018-08-27 14:14:37 -0700

    generate paranthesis

---
+++ graphs/generate_parenthesis/method.py
@@ -0,0 +1,89 @@
+from stacks.stack import Stack
+
+OPEN = '('
+CLOSE = ')'
+
+# Understanding:
+#   Valid: (())()
+#   Invalid: )(()()
+#   Valid: every parenthsis to the left is matched with one to the right
+#   Generating combination:
+#       [)*, (] -> [(), ((] -> [()(, ())*, (((, (()]
+#   Counting:
+#       n!/((n / 2)! + (n / 2)!) minus invalid states
+# Problem:
+#   Given a number n, return an array of strings,
+#       each string representing a valid combination of n sets of parenthesis
+# Plan:
+#   Graph:
+#       ['(' or ')'] -> ['(' or ')']
+#   Generate the graph of all possible combinations
+#       Maintain a stack of ( and of )
+#       Bulid tree from popping from each stack
+#       Perform a depth first search to reach valid paranthetical expressions
+#           invalidate impossible paths
+#               push thing back onto stack if invalid
+#           when n*2 in length is reach, push into results
+#           Each node should receive its own string
+#   Maintain a results array
+
+def generate_parenthesis(n):
+    results, open, close = [], Stack(), Stack()
+
+    for i in range(0, n * 2):
+        open.push(OPEN) if i % 2 else close.push(CLOSE)
+
+    def generate(expr):
+        if len(expr) and cannot_be_valid(expr):
+            return
+
+        if open.is_empty() and close.is_empty():
+            results.append(expr)
+            return
+
+        if not open.is_empty():
+            open.pop()
+            generate(expr + OPEN)
+            open.push(OPEN)
+
+        if not close.is_empty():
+            close.pop()
+            generate(expr + CLOSE)
+            close.push(CLOSE)
+
+    generate('')
+
+    return results
+
+
+# Understanding:
+#   A balanced parenthetical expression has no closing tag without a corresponding opening
+# Problem:
+#   Identify a closing paren towards on the right that has no match on the left
+# Plan:
+#   Iterate from right to left
+#       Push closing paren onto a stack
+#       Pop stack when open paren is reached
+#       return stack == empty
+def cannot_be_valid(expr):
+    i, close = len(expr) - 1, Stack()
+
+    while i >= 0:
+        char = expr[i]
+
+        if char == CLOSE:
+            close.push(char)
+
+        else:
+            try:
+                close.pop()
+            # an opening paren was reached with no closing tags to the right of it
+            except IndexError:
+                return False
+
+        i -= 1
+
+    return not close.is_empty()
+
+
+
---
+++ graphs/generate_parenthesis/test.py
@@ -0,0 +1,13 @@
+from graphs.generate_parenthesis.method import generate_parenthesis
+# from graphs.anagram.test import factorial
+
+# def count_valid_expressions(n):
+#     duplicate_open_permutations = factorial(n)
+#     duplicaate_close_permutations = duplicate_open_permutations
+
+#     return factorial(n * 2) / (duplicaate_close_permutations + duplicate_open_permutations)
+
+
+print(generate_parenthesis(3))
+
+# assert expected == len(received), "Expected {} exprs, but received {}".format(expected, received)
