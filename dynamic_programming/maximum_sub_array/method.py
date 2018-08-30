a38de5c9f2feeefbb2421b46957ca6a7636c9af4
Jonathan Milgrom <jbmilgrom@gmail.com>
2018-08-30 14:23:11 -0700

    i guess

--- dynamic_programming/maximum_sub_array/method.py
+++ dynamic_programming/maximum_sub_array/method.py
@@ -4,7 +4,42 @@
 #   If all numbers are negative, the largest subarray is the least negative number by itself
 #   n^2 iteration could build each sum
 #   The max of (the max subarray of each of 0...n-1) gives the answer
-# def maximum_subarray(arr):
+# Insight:
+#   Find the maximum subarray that ends at i
+#   Add i + 1
+# Understanding:
+#   The maximum subarray that ends at i + 1 is:
+#       the maximum subarrary plus i if i > 0 and array ends at i
+#       elif i < 0: do nothing
+#       elif subarray[i] == None: check subarray ending at i against max
+# Problem:
+#   Given an array of integers, return the max sum of aany contiguous subarray
+def maximum_subarray(arr):
+    length = len(arr)
+
+    def maximum_subarray_that_ends_at(i, max, end, adjacent_max):
+        if i == length:
+            return max
+
+        num, next = arr[i], i + 1
+
+        if max < 0 and num > max:
+            return maximum_subarray_that_ends_at(next, num, i, None)
+
+        if num < 0:
+            return maximum_subarray_that_ends_at(next, max, end, None)
+
+        if end == i - 1:
+            return maximum_subarray_that_ends_at(next, max + num, i, None)
+
+        candidate = num if adjacent_max is None else adjacent_max + num
+
+        if candidate > max:
+            return maximum_subarray_that_ends_at(next, candidate, i, candidate)
+        else:
+            return maximum_subarray_that_ends_at(next, max, end, candidate)
+
+    return maximum_subarray_that_ends_at(1, arr[0], 0, None)
 
 
-# def maximum_subarray_that_ends_at(i, arr):
+
---
+++ dynamic_programming/maximum_sub_array/test.py
@@ -0,0 +1,17 @@
+from dynamic_programming.maximum_sub_array.method import maximum_subarray
+
+print('############################')
+print('Testing maximum_sub_array')
+print('############################')
+
+max = maximum_subarray([-1, 3,2,6, -3, 4,5])
+assert max == 3 + 2 + 6, "expected {}; received: {}".format(11, max)
+
+max = maximum_subarray([-1, -3, -2, -6, -3, -4, -5])
+assert max == -1, "expected {}; received: {}".format(-1, max)
+
+max = maximum_subarray([-9, -3, -2, -6, -3, -4, -5])
+assert max == -2, "expected {}; received: {}".format(-2, max)
+
+max = maximum_subarray([-1, 3,2,6, -3, 4,5, 9, -1])
+assert max == 18, "expected {}; received: {}".format(18, max)
