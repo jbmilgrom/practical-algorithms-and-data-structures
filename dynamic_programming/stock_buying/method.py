0fe8ab26a2ac3aac392dd47faebbd97adef7ee27
Jonathan Milgrom <jbmilgrom@gmail.com>
2018-09-04 17:16:42 -0700

    stock buying start

--- dynamic_programming/stock_buying/method.py
+++ dynamic_programming/stock_buying/method.py
@@ -46,20 +46,20 @@ def max_profit_dfs(prices):
 
             search(i + 1, next, profit, purchase_price)
 
-    search(0, Graph.NO_STOCK, 0, None)
+    search(0, Graph.DRY_POWDER, 0, None)
 
     return maximum
 
 class Graph:
-    NO_STOCK = 'NO_STOCK'
+    DRY_POWDER = 'DRY_POWDER'
     HOLDING = 'HOLDING'
     COOLDOWN = 'COOLDOWN'
 
     def __init__(self):
         self._GRAPH = {
-            self.NO_STOCK: [self.HOLDING, self.NO_STOCK],
+            self.DRY_POWDER: [self.HOLDING, self.DRY_POWDER],
             self.HOLDING: [self.COOLDOWN, self.HOLDING],
-            self.COOLDOWN: [self.NO_STOCK]
+            self.COOLDOWN: [self.DRY_POWDER]
         }
 
     def neighbors(self, STATE):
@@ -69,5 +69,80 @@ class Graph:
         return prev is self.HOLDING and next is self.COOLDOWN
 
     def is_purchase(self, next, prev):
-        return prev is self.NO_STOCK and next is self.HOLDING
+        return prev is self.DRY_POWDER and next is self.HOLDING
 
+
+# Understanding:
+#   As discussed above, there are 3 possible states to be in at any point in time
+#       HOLDING, COOLDOWN, DRY_POWDER
+#   Insight: All we have to do is track the best way to get to each state at price i
+#       The best way to get to any state at price i will both
+#           i. provide maximum profit at end of iteration i.e. max(COOLDOWN, DRY_POWDER)
+#           ii. provide enough information at every price i to deteminat the best scenarios for price i + 1
+#   Since we are iterating from left to right and the above Adjacency Graph points to next states
+#       we need to invert the graph to get _previous_ states:
+#           DRY_POWDER: [DRY_POWDER, COOLDOWN]
+#           HOLDING: [HOLDING, DRY_POWDER]
+#           COOLDOWN: [HOLDING]
+# Plan:
+#   dry_powder[n] = max(dry_powder[n-1], cooldown[n-1])
+#   holding[n] = max(dry_powder[n-1] - price[n], holding[n-1])
+#   cooldown[n] = holding[n-1] + price[n]
+
+def max_profit_dp(prices):
+    if not len(prices):
+        return 0
+
+    powder, holding, cooldown = 0, -prices[0], float('-inf')
+
+    for i, price in enumerate(prices):
+        if i == 0:
+            continue
+
+        powder, holding, cooldown = max(powder, cooldown), max(powder - price, holding), holding + price
+
+    return max(powder, cooldown)
+
+# Problem: given an array of prices, buy and sell once to maximime profit
+#   return 0 if no profit can be made
+# Understanding:
+#   (NO_STOCK) -> (NO_STOCK, HOLDING) -> ((NO_STOCK, HOLDING), (HOLDING, SOLD))
+#   Brute Force:
+#       iterate from left to right over every price i,
+#           iterate from left to right for each j > i,
+#               calc the max price[j] - price[i]
+#   State machine:
+#       no_stock[i] = no_stock[i - 1]
+#       holding[i] = max(-price[i] + no_stock[i-1], holding[i-1])
+#       sold[i] = holding[i-1] + price[i]
+#   Insight:
+#       Selling at price i exhausts all possible sales
+# Plan:
+#   iterate from left to right over every price i
+#       maintain the min price
+#       maintain the max sale
+#   return the max sale
+
+def max_profit_single_transaction_dp(prices):
+    min_price, profit = prices[0], 0
+
+    for i, price in enumerate(prices):
+        if i is 0:
+            continue
+
+        profit = max(profit, price - min_price)
+        min_price = min(min_price, price)
+
+    return profit
+
+def max_profit_single_transaction_dp_states(prices):
+    holding, sold = -prices[0], 0
+
+    for i, price in enumerate(prices):
+        if i is 0:
+            continue
+
+        sold = max(sold, price + holding)
+        holding = max(holding, -price)
+
+    return sold
--- dynamic_programming/stock_buying/test.py
+++ dynamic_programming/stock_buying/test.py
@@ -1,11 +1,37 @@
-from dynamic_programming.stock_buying.method import max_profit_dfs
+from dynamic_programming.stock_buying.method import max_profit_dfs, max_profit_dp, max_profit_single_transaction_dp
+from dynamic_programming.stock_buying.method import max_profit_single_transaction_dp_states
 
 print('############################')
-print('Testing Stock Buying')
+print('Testing Stock Buying with Cooldown')
 print('############################')
 
 prices = [1,2,3,0,2]
-
 profit = max_profit_dfs(prices)
 assert profit == 3, "Expected profit: {}; received {}".format(3, profit)
 
+prices = [1,2,3,0,2]
+profit = max_profit_dp(prices)
+assert profit == 3, "Expected profit: {}; received {}".format(3, profit)
+
+print('############################')
+print('Testing Stock Buying: singly buy and sell')
+print('############################')
+
+def max_profit_single_transaction_bf(prices):
+    profit, numPrices = 0, len(prices)
+
+    for i, price in enumerate(prices):
+        for j in range(i + 1, numPrices):
+            profit = max(profit, prices[j] - price)
+
+    return profit
+
+prices_collection = [[1,2,3,0,1], [1,2,1,10,5], [10,2,3,0,2], [10,9,3,2,1]]
+
+for prices in prices_collection:
+    brute, dp = max_profit_single_transaction_bf(prices), max_profit_single_transaction_dp(prices)
+    assert brute == dp, "Expected {}; Received: {}".format(brute, dp)
+
+for prices in prices_collection:
+    brute, dp = max_profit_single_transaction_bf(prices), max_profit_single_transaction_dp_states(prices)
+    assert brute == dp, "Expected {}; Received: {}".format(brute, dp)
