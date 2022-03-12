655f418a88120ddf2cd9a534379308bc417f5f1d
Jonathan Milgrom <jmilgrom@snapchat.com>
2022-03-12 07:45:18 -0800

    add BFS

--- graphs/maze.ts
+++ graphs/maze.ts
@@ -12,7 +12,7 @@ type Cooridinate = [number, number];
 
 type Visited = Set<string>;
 
-const navigateMaze = (maze: Maze): Cooridinate | undefined => {
+const navigateMazeDFS = (maze: Maze): Cooridinate | undefined => {
   const visited: Visited = new Set();
   const xBoundary = maze[0].length;
   const yBoundary = maze.length;
@@ -43,6 +43,36 @@ const navigateMaze = (maze: Maze): Cooridinate | undefined => {
   return search([0, 0]);
 };
 
+const navigateMazeBFS = (maze: Maze): Cooridinate | undefined => {
+  const visited: Visited = new Set();
+  const xBoundary = maze[0].length;
+  const yBoundary = maze.length;
+
+  const queue = Queue<Cooridinate>();
+  queue.enqueue([0, 0]);
+
+  while (!queue.isEmpty()) {
+    const coordinate = queue.dequeue() as Cooridinate;
+    if (isVisited(visited, coordinate)) {
+      continue;
+    }
+    setVisited(visited, coordinate);
+
+    const value = getValue(maze, coordinate);
+    if (value === 9) {
+      return coordinate;
+    }
+    if (value === 5) {
+      continue;
+    }
+    const nextCoordinates = get3by3SquareAt(coordinate, [xBoundary, yBoundary]);
+
+    for (const nextCoordinate of nextCoordinates) {
+      queue.enqueue(nextCoordinate);
+    }
+  }
+};
+
 const get3by3SquareAt = ([x, y]: Cooridinate, [xBoundary, yBoundary]: Cooridinate): Cooridinate[] => {
   const coordinates: Cooridinate[] = [];
   for (let xOffset = -1; xOffset <= 1; xOffset++) {
@@ -66,4 +96,14 @@ const setVisited = (visited: Visited, coordinate: Cooridinate): Visited => visit
 
 const getVisitedKey = ([x, y]: Cooridinate): string => `${x}:${y}`;
 
-console.log(navigateMaze(MAZE_1));
+const Queue = <T>() => {
+  const array: T[] = [];
+  return {
+    enqueue: (item: T) => array.push(item),
+    dequeue: (): T | undefined => array.shift(),
+    isEmpty: (): boolean => array.length === 0,
+  };
+};
+
+console.log(navigateMazeDFS(MAZE_1));
+console.log(navigateMazeBFS(MAZE_1));
