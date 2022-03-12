b797a263356803419e2f26bd586e16a3d772742a
Jonathan Milgrom <jmilgrom@snapchat.com>
2022-03-12 07:13:54 -0800

    prettier

--- graphs/maze.ts
+++ graphs/maze.ts
@@ -1,9 +1,9 @@
 const MAZE_1 = [
-    [ 1, 5, 0, 0, 0, ],
-    [ 0, 5, 0, 5, 5, ],
-    [ 0, 5, 0, 0, 0, ],
-    [ 0, 5, 0, 5, 0, ],
-    [ 0, 0, 0, 5, 9, ],
+  [1, 5, 0, 0, 0],
+  [0, 5, 0, 5, 5],
+  [0, 5, 0, 0, 0],
+  [0, 5, 0, 5, 0],
+  [0, 0, 0, 5, 9],
 ] as const;
 
 type Maze = typeof MAZE_1;
@@ -13,51 +13,50 @@ type Cooridinate = [number, number];
 type Visited = Set<string>;
 
 const navigateMaze = (maze: Maze) => {
+  const visited: Visited = new Set();
+  const xBoundary = maze[0].length;
+  const yBoundary = maze.length;
 
-    const visited: Visited = new Set();
-    const xBoundary = maze[0].length;
-    const yBoundary = maze.length;
+  const search = (coordinate: Cooridinate): Cooridinate | undefined => {
+    if (isVisited(visited, coordinate)) {
+      return;
+    }
+    setVisited(visited, coordinate);
 
-    const search = (coordinate: Cooridinate): Cooridinate | undefined => {
-        if (isVisited(visited, coordinate)) {
-            return;
-        }
-        setVisited(visited, coordinate);
+    const value = getValue(maze, coordinate);
+    if (value === 5) {
+      return;
+    }
+    if (value === 9) {
+      return coordinate;
+    }
 
-        const value = getValue(maze, coordinate);
-        if (value === 5) {
-            return;
-        }
-        if (value === 9) {
-            return coordinate;
-        }
-        
-        const nextCoordinates = get3by3SquareAt(coordinate, [xBoundary, yBoundary]);
-        for (const nextCoordinate of nextCoordinates) {
-            const result = search(nextCoordinate);
-            if (result !== undefined) {
-                return result;
-            }
-        }
-    };
+    const nextCoordinates = get3by3SquareAt(coordinate, [xBoundary, yBoundary]);
+    for (const nextCoordinate of nextCoordinates) {
+      const result = search(nextCoordinate);
+      if (result !== undefined) {
+        return result;
+      }
+    }
+  };
 
-    return search([0, 0]);
+  return search([0, 0]);
 };
 
 const get3by3SquareAt = ([x, y]: Cooridinate, [xBoundary, yBoundary]: Cooridinate): Cooridinate[] => {
-    const coordinates: Cooridinate[] = [];
-    for (let xOffset = -1; xOffset <= 1; xOffset++) {
-        for (let yOffset = -1; yOffset <= 1; yOffset++) {
-            const cX = x + xOffset;
-            const cY = y + yOffset;
-            if (cX < 0 || cY < 0 || cX >= xBoundary || cY >= yBoundary) {
-                continue;
-            }
-            coordinates.push([cX, cY]);
-        }
+  const coordinates: Cooridinate[] = [];
+  for (let xOffset = -1; xOffset <= 1; xOffset++) {
+    for (let yOffset = -1; yOffset <= 1; yOffset++) {
+      const cX = x + xOffset;
+      const cY = y + yOffset;
+      if (cX < 0 || cY < 0 || cX >= xBoundary || cY >= yBoundary) {
+        continue;
+      }
+      coordinates.push([cX, cY]);
     }
-    return coordinates;
-}
+  }
+  return coordinates;
+};
 
 const getValue = (maze: Maze, [x, y]: Cooridinate) => maze[y][x];
 
@@ -66,6 +65,5 @@ const isVisited = (visited: Visited, coordinate: Cooridinate): boolean => visite
 const setVisited = (visited: Visited, coordinate: Cooridinate): Visited => visited.add(getVisitedKey(coordinate));
 
 const getVisitedKey = ([x, y]: Cooridinate): string => `${x}:${y}`;
-
 
 console.log(navigateMaze(MAZE_1));
